c8434a27d6c8af0e82085a3a08f688f778ab5d82
a rahni <rahniahmed@gmail.com>
2022-07-27 11:29:57 +0200

    exo connexion

---
+++ src/main/java/fr/m2i/javawebapp/LogoutServlet.java
@@ -0,0 +1,65 @@
+
+package fr.m2i.javawebapp;
+
+import fr.m2i.javawebapp.session.User;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author rahni
+ */
+public class LogoutServlet extends HttpServlet {
+
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        super.doGet(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //super.doPost(request, response);
+        HttpSession session = request.getSession();
+        String userSession= session.getAttribute("user").toString();
+        System.out.println(" fermer session: "+ userSession);
+        session.invalidate();
+        //request.setAttribute("logoutMessage", "vous êtes deconnecté");
+        this.getServletContext().getRequestDispatcher("/WEB-INF/logout.jsp").forward(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
